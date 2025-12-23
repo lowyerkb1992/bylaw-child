45c68ef71096d9605e99c4f087847add07ffab1b
lowyerkb1992 <lowyerkb1992@gmail.com>
2025-12-23 18:13:37 +0400

    Add main header template part

---
+++ template-parts/header/main-header.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Main Header
+ */
+?>
+
+<div class="main-header">
+  <div class="main-header__inner">
+    <!-- هنا اللوجو -->
+    <!-- هنا المينيو -->
+    <!-- هنا زر التواصل -->
+  </div>
+</div>
