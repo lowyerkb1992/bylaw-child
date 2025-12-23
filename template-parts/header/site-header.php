fe7d95223305d99d05a3872171d3c30cd513a169
lowyerkb1992 <lowyerkb1992@gmail.com>
2025-12-23 18:01:26 +0400

    Fix site header structure

--- template-parts/header/site-header.php
+++ template-parts/header/site-header.php
@@ -3,3 +3,9 @@
  * Site Header
  * Loaded via get_template_part( 'template-parts/header/site-header' )
  */
+?>
+
+<header id="site-header" class="site-header">
+  <?php get_template_part( 'template-parts/header/topbar' ); ?>
+  <?php get_template_part( 'template-parts/header/main-header' ); ?>
+</header>
