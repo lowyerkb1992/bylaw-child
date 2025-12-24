9c92f238f79decdf1ef53207528b98f26dbcaec2
lowyerkb1992 <lowyerkb1992@gmail.com>
2025-12-24 11:45:13 +0400

    header logo (logo.svg)
    Added logo display to the main header.

--- template-parts/header/main-header.php
+++ template-parts/header/main-header.php
@@ -1,13 +1,18 @@
 <?php
 /**
- * Main Header
+ * Main Header Logo
  */
+
+// مسار اللوجو (من داخل القالب)
+$logo_url = trailingslashit( get_template_directory_uri() ) . 'assets/images/logo.svg';
 ?>
 
-<div class="main-header">
-  <div class="main-header__inner">
-    <!-- هنا اللوجو -->
-    <!-- هنا المينيو -->
-    <!-- هنا زر التواصل -->
-  </div>
+<div class="site-logo">
+  <a href="<?php echo esc_url( home_url('/') ); ?>" aria-label="<?php echo esc_attr( get_bloginfo('name') ); ?>">
+    <img
+      src="<?php echo esc_url( $logo_url ); ?>"
+      alt="<?php echo esc_attr( get_bloginfo('name') ); ?>"
+      loading="lazy"
+    >
+  </a>
 </div>
